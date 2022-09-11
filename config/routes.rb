f257eba2f1ab1c0340f6f3542e40f218cbaf9407
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-09-11 07:50:57 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers ,skip: [:password], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admins,skip: [:registrations, :password], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  root 'home#top'
+  get 'home/about', to: 'home#about'
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20220910135821_create_order_datails.rb
@@ -0,0 +1,13 @@
+class CreateOrderDatails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_datails do |t|
+      t.integer :item_id, null: false, default: ""
+      t.integer :order_id, null: false, default: ""
+      t.integer :purchase_price, null: false, default: ""
+      t.integer :amount, null: false, default: ""
+      t.integer :make_status, null: false, default: "0"
+
+      t.timestamps
+    end
+  end
+end
