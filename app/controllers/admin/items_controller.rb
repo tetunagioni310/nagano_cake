597b03fa444f32ee32cb2926429d0ea4381fab33
tetunagioni310 <104610603+tetunagioni310@users.noreply.github.com>
2022-09-16 23:11:27 +0000

    [add]items edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -13,19 +13,27 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_items_path
   end
-  
+
   def destroy
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path
+  end
+
   private
-  
+
   def item_params
     params.require(:item).permit(:name,:introduction,:price,:is_active,:genre_id,:image)
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  
-  def add_tax_price
-    (self.price * 1.10).round
-  end
+
 end
